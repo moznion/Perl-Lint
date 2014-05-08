1bc2f8abd115f2940c3ff4087743e7e9b4039c49
moznion <moznion@gmail.com>
2014-05-08 23:53:28 +0900

    Implement RequireCarping

--- lib/Perl/Lint/Evaluator/Constants/Type.pm
+++ lib/Perl/Lint/Evaluator/Constants/Type.pm
@@ -5,21 +5,28 @@ use Compiler::Lexer::Constants;
 use parent qw/Exporter/;
 
 our @EXPORT = qw(
-    VAR_DECL OUR_DECL
+    VAR_DECL OUR_DECL FUNCTION_DECL
     VAR CODE_VAR ARRAY_VAR HASH_VAR
     GLOBAL_VAR GLOBAL_ARRAY_VAR GLOBAL_HAHS_VAR
     LOCAL_VAR LOCAL_ARRAY_VAR LOCAL_HASH_VAR
-    SEMI_COLON
+    SEMI_COLON COMMA
     ASSIGN
-    LEFT_BRACE
-    RIGHT_PAREN
-    KEY
-    IF_STATEMENT UNLESS_STATEMENT FOR_STATEMENT FOREACH_STATEMENT WHILE_STATEMENT
+    RIGHT_BRACE LEFT_BRACE
+    RIGHT_PAREN LEFT_PAREN
+    KEY METHOD
+    STRING RAW_STRING
+    HERE_DOCUMENT RAW_HERE_DOCUMENT HERE_DOCUMENT_END HERE_DOCUMENT_TAG HERE_DOCUMENT_RAW_TAG
+    REG_QUOTE REG_DOUBLE_QUOTE
+    REG_DELIM
+    IF_STATEMENT UNLESS_STATEMENT FOR_STATEMENT FOREACH_STATEMENT WHILE_STATEMENT UNTIL_STATEMENT
+    BUILTIN_FUNC
+    PACKAGE
 );
 
 use constant {
-    VAR_DECL   => Compiler::Lexer::TokenType::T_VarDecl,
-    OUR_DECL   => Compiler::Lexer::TokenType::T_OurDecl,
+    VAR_DECL      => Compiler::Lexer::TokenType::T_VarDecl,
+    OUR_DECL      => Compiler::Lexer::TokenType::T_OurDecl,
+    FUNCTION_DECL => Compiler::Lexer::TokenType::T_FunctionDecl,
 
     VAR       => Compiler::Lexer::TokenType::T_Var,
     CODE_VAR  => Compiler::Lexer::TokenType::T_CodeVar,
@@ -39,14 +46,36 @@ use constant {
     WHILE_STATEMENT   => Compiler::Lexer::TokenType::T_WhileStmt,
     FOR_STATEMENT     => Compiler::Lexer::TokenType::T_ForStmt,
     FOREACH_STATEMENT => Compiler::Lexer::TokenType::T_ForeachStmt,
+    UNTIL_STATEMENT   => Compiler::Lexer::TokenType::T_UntilStmt,
 
+    BUILTIN_FUNC => Compiler::Lexer::TokenType::T_BuiltinFunc,
+
+    RIGHT_BRACE => Compiler::Lexer::TokenType::T_RightBrace,
     LEFT_BRACE  => Compiler::Lexer::TokenType::T_LeftBrace,
     RIGHT_PAREN => Compiler::Lexer::TokenType::T_RightParenthesis,
+    LEFT_PAREN  => Compiler::Lexer::TokenType::T_LeftParenthesis,
 
-    KEY => Compiler::Lexer::TokenType::T_Key,
+    METHOD     => Compiler::Lexer::TokenType::T_Method,
+    KEY        => Compiler::Lexer::TokenType::T_Key,
+    STRING     => Compiler::Lexer::TokenType::T_String,
+    RAW_STRING => Compiler::Lexer::TokenType::T_RawString,
 
+    HERE_DOCUMENT         => Compiler::Lexer::TokenType::T_HereDocument,
+    RAW_HERE_DOCUMENT     => Compiler::Lexer::TokenType::T_RawHereDocument,
+    HERE_DOCUMENT_TAG     => Compiler::Lexer::TokenType::T_HereDocumentTag,
+    HERE_DOCUMENT_RAW_TAG => Compiler::Lexer::TokenType::T_HereDocumentRawTag,
+    HERE_DOCUMENT_END     => Compiler::Lexer::TokenType::T_HereDocumentEnd,
+
+    REG_QUOTE        => Compiler::Lexer::TokenType::T_RegQuote,
+    REG_DOUBLE_QUOTE => Compiler::Lexer::TokenType::T_RegDoubleQuote,
+
+    REG_DELIM => Compiler::Lexer::TokenType::T_RegDelim,
+
+    COMMA      => Compiler::Lexer::TokenType::T_Comma,
     SEMI_COLON => Compiler::Lexer::TokenType::T_SemiColon,
     ASSIGN     => Compiler::Lexer::TokenType::T_Assign,
+
+    PACKAGE => Compiler::Lexer::TokenType::T_Package,
 };
 
 1;
---
+++ lib/Perl/Lint/Evaluator/ErrorHandling/RequireCarping.pm
@@ -0,0 +1,169 @@
+package Perl::Lint::Evaluator::ErrorHandling::RequireCarping;
+use strict;
+use warnings;
+use Perl::Lint::Evaluator::Constants::Type;
+use parent "Perl::Lint::Evaluator";
+
+# TODO msg!
+use constant {
+    DESC => q{Don't complain about die or warn if the message ends in a newline.},
+    EXPL => '',
+};
+
+sub evaluate {
+    my ($class, $file, $tokens, $args) = @_;
+
+    my $options = $args->{require_carping};
+    my $allow_messages_ending_with_newlines = 1;
+    if (defined $options->{allow_messages_ending_with_newlines}) {
+        $allow_messages_ending_with_newlines =
+            $options->{allow_messages_ending_with_newlines};
+    }
+    my $allow_in_main_unless_in_subroutine =
+        $options->{allow_in_main_unless_in_subroutine } || 0;
+
+    my $is_in_main = 1;
+    my $is_in_sub  = 0;
+
+    my $left_brace_num = 0;
+
+    my @violations;
+    my $token_num = scalar @$tokens;
+
+    for (my $i = 0; $i < $token_num; $i++) {
+        my $token      = $tokens->[$i];
+        my $token_type = $token->{type};
+        my $token_data = $token->{data};
+
+        if (
+            $token_type eq BUILTIN_FUNC &&
+            ($token_data eq 'die' || $token_data eq 'warn')
+        ) {
+            my %last_msg;
+            for ($i++; $i <= $token_num; $i++) {
+                $token      = $tokens->[$i];
+                $token_type = $token->{type};
+                $token_data = $token->{data};
+
+                no warnings qw/uninitialized/;
+                if ($token_type == STRING) {
+                    %last_msg = (type => 'not_raw', data => $token_data);
+                }
+                elsif ($token_type == REG_DOUBLE_QUOTE) {
+                    %last_msg = (type => 'not_raw', data => $tokens->[$i+=2]->{data});
+                }
+                elsif ($token_type == RAW_STRING) {
+                    %last_msg = (type => 'raw', data => $token_data);
+                }
+                elsif ($token_type == REG_QUOTE) {
+                    %last_msg = (type => 'raw', data => $tokens->[$i+=2]->{data});
+                }
+                elsif ($token_type == HERE_DOCUMENT_TAG || $token_type == HERE_DOCUMENT_RAW_TAG) {
+                    %last_msg = (type => 'heredoc', data => $token_data);
+                }
+                elsif (
+                    $i + 1 >= $token_num             ||
+                    $token_type == SEMI_COLON        ||
+                    $token_type == IF_STATEMENT      ||
+                    $token_type == UNLESS_STATEMENT  ||
+                    $token_type == WHILE_STATEMENT   ||
+                    $token_type == FOR_STATEMENT     ||
+                    $token_type == FOREACH_STATEMENT ||
+                    $token_type == UNTIL_STATEMENT   ||
+                    $token_type == HERE_DOCUMENT_END
+                ) {
+                    my $last_msg_type = $last_msg{type};
+                    my $last_msg_data = $last_msg{data};
+
+                    if(
+                        !(defined $last_msg_type && defined $last_msg_data) ||
+                        ($last_msg_type eq 'raw' && (substr($last_msg_data, -1) ne "\n" || !$allow_messages_ending_with_newlines)) ||
+                        ($last_msg_type eq 'not_raw' && ($last_msg_data !~ /(?:\\n|\n)\Z/ || !$allow_messages_ending_with_newlines))
+                    ) {
+                        if ($is_in_sub || !($is_in_main && $allow_in_main_unless_in_subroutine)) {
+                            push @violations, {
+                                filename => $file,
+                                line     => $token->{line},
+                                description => DESC,
+                                explanation => EXPL,
+                            };
+                        }
+                    }
+                    last;
+                }
+                elsif ($token_type == METHOD) {
+                    $i++; # Skip a left parenthesis
+                    my $left_paren_num = 1;
+                    for ($i++; $i < $token_num; $i++) {
+                        my $token_type = $tokens->[$i]->{type};
+
+                        if ($token_type == RIGHT_PAREN) {
+                            $left_paren_num--;
+                        }
+                        elsif ($token_type == LEFT_PAREN) {
+                            $left_paren_num++;
+                        }
+
+                        if ($left_paren_num <= 0) {
+                            last;
+                        }
+                    }
+                }
+                elsif (
+                    $token_type == BUILTIN_FUNC ||
+                    $token_type == KEY
+                ) {
+                    my $left_paren_num = 0;
+                    for ($i++; $i < $token_num; $i++) {
+                        my $token_type = $tokens->[$i]->{type};
+
+                        if ($token_type == RIGHT_PAREN) {
+                            $left_paren_num--;
+                        }
+                        elsif ($token_type == LEFT_PAREN) {
+                            $left_paren_num++;
+                        }
+
+                        if ($left_paren_num <= 0) {
+                            last;
+                        }
+                    }
+                }
+                elsif (
+                    $token_type != REG_DELIM     &&
+                    $token_type != COMMA         &&
+                    $token_type != RIGHT_PAREN   &&
+                    $token_type != HERE_DOCUMENT &&
+                    $token_type != RAW_HERE_DOCUMENT
+                ) {
+                    %last_msg = ();
+                }
+                elsif ($token_type == PACKAGE) {
+                    $is_in_main = $tokens->[++$i]->{data} eq 'main' ? 1 : 0;
+                }
+
+                use warnings;
+            }
+        }
+        elsif ($token_type == PACKAGE) {
+            $is_in_main = $tokens->[++$i]->{data} eq 'main' ? 1 : 0;
+        }
+        elsif ($token_type == FUNCTION_DECL) {
+            $is_in_sub = 1;
+        }
+        elsif ($token_type == LEFT_BRACE) {
+            $left_brace_num++;
+        }
+        elsif ($token_type == RIGHT_BRACE) {
+            $left_brace_num--;
+            if ($left_brace_num <= 0) {
+                $is_in_sub = 0;
+            }
+        }
+    }
+
+    return \@violations;
+}
+
+1;
+
