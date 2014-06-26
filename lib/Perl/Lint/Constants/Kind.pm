4dff609334e5af23a65990be896df8a92b668ee7
moznion <moznion@gmail.com>
2014-06-26 22:46:18 +0900

    Implement ProhibitTwoArgOpen

--- lib/Perl/Lint/Constants/Kind.pm
+++ lib/Perl/Lint/Constants/Kind.pm
@@ -5,13 +5,19 @@ use Compiler::Lexer::Constants;
 use parent qw/Exporter/;
 
 our @EXPORT = qw(
+    KIND_CTRL
     KIND_DECL
+    KIND_OP
     KIND_STMT
+    KIND_STMT_END
 );
 
 use constant {
+    KIND_CTRL => Compiler::Lexer::Kind::T_Control,
     KIND_DECL => Compiler::Lexer::Kind::T_Decl,
+    KIND_OP   => Compiler::Lexer::Kind::T_Operator,
     KIND_STMT => Compiler::Lexer::Kind::T_Stmt,
+    KIND_STMT_END => Compiler::Lexer::Kind::T_StmtEnd,
 };
 
 1;
