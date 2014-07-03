ee1e2c2675ec18f8a5cc8515804c98b05404027d
moznion <moznion@gmail.com>
2014-07-03 19:09:04 +0900

    Implement ProhibitLvalueSubstr

---
+++ lib/Perl/Lint/Evaluator/BuiltinFunctions/ProhibitLvalueSubstr.pm
@@ -0,0 +1,61 @@
+package Perl::Lint::Evaluator::BuiltinFunctions::ProhibitLvalueSubstr;
+use strict;
+use warnings;
+use Perl::Lint::Constants::Type;
+use parent "Perl::Lint::Evaluator";
+
+# TODO msg!
+use constant {
+    DESC => '',
+    EXPL => '',
+};
+
+sub evaluate {
+    my ($class, $file, $tokens, $src, $args) = @_;
+
+    my @violations;
+    for (my $i = 0; my $token = $tokens->[$i]; $i++) {
+        my $token_type = $token->{type};
+        my $token_data = $token->{data};
+
+        if ($token_type == BUILTIN_FUNC && $token_data eq 'substr') {
+            $token = $tokens->[++$i];
+            if ($token->{type} == LEFT_PAREN) {
+                my $left_paren_num = 1;
+                for ($i++; my $token = $tokens->[$i]; $i++) {
+                    $token = $tokens->[++$i];
+                    $token_type = $token->{type};
+
+                    if ($token_type == LEFT_PAREN) {
+                        $left_paren_num++;
+                    }
+                    elsif ($token_type == RIGHT_PAREN) {
+                        if (--$left_paren_num <= 0) {
+                            my $next_token = $tokens->[++$i];
+                            if ($next_token->{type} == ASSIGN) {
+                                push @violations, {
+                                    filename => $file,
+                                    line     => $token->{line},
+                                    description => DESC,
+                                    explanation => EXPL,
+                                };
+                            }
+                            last;
+                        }
+                    }
+                }
+            }
+        }
+        elsif ($token_type == USE_DECL) {
+            $token = $tokens->[++$i];
+            if ($token->{type} == DOUBLE && $token->{data} <= 5.004) {
+                return [];
+            }
+        }
+    }
+
+    return \@violations;
+}
+
+1;
+
