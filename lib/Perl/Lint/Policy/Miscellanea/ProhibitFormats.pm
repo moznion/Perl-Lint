1296d313cb7518cef513bfccc42453eab363821b
moznion <moznion@gmail.com>
2014-08-12 02:59:48 +0900

    Avoid infinity loops

--- lib/Perl/Lint/Policy/Miscellanea/ProhibitFormats.pm
+++ lib/Perl/Lint/Policy/Miscellanea/ProhibitFormats.pm
@@ -13,9 +13,10 @@ sub evaluate {
     my ($class, $file, $tokens, $args) = @_;
 
     my @violations;
-    for (my $i = 0; my $token = $tokens->[$i]; $i++) {
+    for (my $i = 0, my $next_token; my $token = $tokens->[$i]; $i++) {
         if ($token->{type} == FORMAT_DECL) {
-            if ($tokens->[$i+1]->{type} != ARROW) {
+            $next_token = $tokens->[$i+1];
+            if ($next_token->{type} != ARROW) {
                 #  XXX workaround for Compiler::Lexer
                 #  ref: https://github.com/goccy/p5-Compiler-Lexer/issues/33
                 push @violations, {
