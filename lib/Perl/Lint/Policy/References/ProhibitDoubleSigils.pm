a6847c5731f57c361da389da0bc6524e4d2226de
moznion <moznion@gmail.com>
2014-08-12 11:40:12 +0900

    Add violation message into References

--- lib/Perl/Lint/Policy/References/ProhibitDoubleSigils.pm
+++ lib/Perl/Lint/Policy/References/ProhibitDoubleSigils.pm
@@ -4,10 +4,9 @@ use warnings;
 use Perl::Lint::Constants::Type;
 use parent "Perl::Lint::Policy";
 
-# TODO msg!
 use constant {
-    DESC => '',
-    EXPL => '',
+    DESC => 'Double-sigil dereference',
+    EXPL => [228],
 };
 
 sub evaluate {
@@ -29,6 +28,7 @@ sub evaluate {
                 line     => $token->{line},
                 description => DESC,
                 explanation => EXPL,
+                policy => __PACKAGE__,
             };
         }
     }
