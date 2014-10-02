9de0ecf144e5a95b4c6db4420138de10c8aebbf4
moznion <moznion@gmail.com>
2014-10-02 15:27:39 +0900

    Implement basic functions for RequireConstantVersion

--- lib/Perl/Lint/Policy/ValuesAndExpressions/RequireConstantVersion.pm
+++ lib/Perl/Lint/Policy/ValuesAndExpressions/RequireConstantVersion.pm
@@ -12,48 +12,175 @@ use constant {
 sub evaluate {
     my ($class, $file, $tokens, $src, $args) = @_;
 
+    my $is_used_version = 0;
+    if (my $this_packages_arg = $args->{require_constant_version}) {
+        $is_used_version = $this_packages_arg->{allow_version_without_use_on_same_line};
+    }
+
     my @violations;
-    for (my $i = 0, my $token_type, my $token_data; my $token = $tokens->[$i]; $i++) {
+    # use Data::Dumper; warn Dumper($tokens);
+
+    my $before_token;
+
+    TOP: for (my $i = 0, my $token_type, my $token_data; my $token = $tokens->[$i]; $i++) {
         $token_type = $token->{type};
+        $token_data = $token->{data};
+
+        # `use version;` declared?
+        if ($token_type == USED_NAME && $token_data eq 'version') {
+            $is_used_version = 1;
+        }
 
         if ($token_type != GLOBAL_VAR && $token_type != VAR) {
             next;
         }
 
-        $token_data = $token->{data};
         if ($token_data ne '$VERSION') {
             next;
         }
 
-        $token = $tokens->[++$i] or last;
-
-        if ($token->{type} != ASSIGN) {
+        $before_token = $tokens->[$i - 1] or next;
+        if ($before_token->{type} == ASSIGN) {
+            # e.g.
+            #   my $foo = $VERSION;
             next;
         }
 
+        my $is_invalid = 0;
+        my $is_var_assigned = 0;
+
+        # check assigning context or not
         for ($i++; $token = $tokens->[$i]; $i++) {
             $token_type = $token->{type};
-            $token_data = $token->{data};
 
-            if ($token_type == SEMI_COLON) {
+            if ($token_type == ASSIGN || $token_type == OR_EQUAL) {
                 last;
             }
-            elsif ($token_type == STRING) {
-                while ($token_data =~ /(\\*)(\$\S+)/gc) {
+            elsif ($token_type == REG_OK) {
+                $is_invalid = 1;
+                last;
+            }
+            elsif ($token_type == SEMI_COLON) {
+                next TOP;
+            }
+        }
+
+        if (!$is_invalid) {
+            for ($i++; $token = $tokens->[$i]; $i++) {
+                $token_type = $token->{type};
+                $token_data = $token->{data};
+
+                if ($token_type == SEMI_COLON) {
+                    last;
+                }
+                elsif ($token_type == STRING) {
+                    if ($is_invalid = $class->_check_interpolation($token_data)) {
+                        last;
+                    }
+                }
+                elsif ($token_type == BUILTIN_FUNC) {
+                    $is_invalid = 1;
+                    last;
+                }
+                elsif ($token_type == REG_DOUBLE_QUOTE) {
+                    $i += 2;
+                    $token = $tokens->[$i] or last;
+                    if ($token->{type} == REG_EXP) {
+                        if ($is_invalid = $class->_check_interpolation($token->{data})) {
+                            last;
+                        }
+                    }
+                }
+                elsif ($token_type == DO) {
+                    $is_invalid = 1;
+                    last;
+                }
+                elsif ($token_type == STRING_MUL) {
+                    $is_invalid = 1;
+                    last;
+                }
+                elsif ($token_type == NAMESPACE) {
+                    $is_invalid = 1;
+                    last;
+                }
+                elsif ($token_type == REG_OK) {
+                    $is_invalid = 1;
+                    last;
+                }
+                elsif ($token_type == LEFT_BRACKET) {
+                    $is_invalid = 1;
+                    last;
+                }
+                elsif ($token_type == VAR || $token_type == GLOBAL_VAR) {
+                    $is_var_assigned = 1;
+                }
+                elsif ($token_type == ASSIGN) {
+                    $is_var_assigned = 0;
+                }
+                elsif ($token_type == KEY) {
+                    if ($token_data eq 'qv') {
+                        # for `qv(...)` notation
+                        if (!$is_used_version) {
+                            $is_invalid = 1;
+                            last;
+                        }
+                    }
+                    elsif ($token_data eq 'version') {
+                        # for `version->new(...)` notation
+
+                        $token = $tokens->[++$i] or last;
+                        if ($token->{type} != POINTER) {
+                            next;
+                        }
+
+                        $token = $tokens->[++$i] or last;
+                        if ($token->{type} != METHOD && $token->{data} ne 'new') {
+                            next;
+                        }
+
+                        if (!$is_used_version) {
+                            $is_invalid = 1;
+                            last;
+                        }
+                    }
+                    else {
+                        $is_invalid = 1;
+                        last;
+                    }
                 }
             }
         }
 
-        # push @violations, {
-        #     filename => $file,
-        #     line     => $token->{line},
-        #     description => DESC,
-        #     explanation => EXPL,
-        #     policy => __PACKAGE__,
-        # };
+        if ($is_invalid || $is_var_assigned) {
+            push @violations, {
+                filename => $file,
+                line     => $token->{line},
+                description => DESC,
+                explanation => EXPL,
+                policy => __PACKAGE__,
+            };
+        }
     }
 
     return \@violations;
+}
+
+sub _check_interpolation {
+    my ($class, $str) = @_;
+
+    while ($str =~ /(\\*)(\$\S+)/gc) {
+        if (length($1) % 2 == 0) {
+            # sigil is not escaped
+            # interpolated!
+            return 1;
+        }
+        else {
+            # sigil is escaped
+            next;
+        }
+    }
+
+    return;
 }
 
 1;
