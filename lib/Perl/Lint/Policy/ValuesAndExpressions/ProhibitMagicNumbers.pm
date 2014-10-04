c7143c8639cb2ac6467b3ea7c1d09c4b9cab13c6
moznion <moznion@gmail.com>
2014-10-04 12:22:08 +0900

    Add skeleton for ProhibitMagicNumbers

---
+++ lib/Perl/Lint/Policy/ValuesAndExpressions/ProhibitMagicNumbers.pm
@@ -0,0 +1,33 @@
+package Perl::Lint::Policy::ValuesAndExpressions::ProhibitMagicNumbers;
+use strict;
+use warnings;
+use Perl::Lint::Constants::Type;
+use parent "Perl::Lint::Policy";
+
+# TODO msg!
+use constant {
+    DESC => '',
+    EXPL => '',
+};
+
+sub evaluate {
+    my ($class, $file, $tokens, $src, $args) = @_;
+
+    my @violations;
+    for (my $i = 0, my $token_type; my $token = $tokens->[$i]; $i++) {
+        $token_type = $token->{type};
+
+        # push @violations, {
+        #     filename => $file,
+        #     line     => $token->{line},
+        #     description => DESC,
+        #     explanation => EXPL,
+        #     policy => __PACKAGE__,
+        # };
+    }
+
+    return \@violations;
+}
+
+1;
+
