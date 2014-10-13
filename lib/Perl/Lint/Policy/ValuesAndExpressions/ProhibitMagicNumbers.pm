a8993008b1ae11b3272081906a907b410b86ddff
moznion <moznion@gmail.com>
2014-10-13 14:10:56 +0900

    Refactoring

--- lib/Perl/Lint/Policy/ValuesAndExpressions/ProhibitMagicNumbers.pm
+++ lib/Perl/Lint/Policy/ValuesAndExpressions/ProhibitMagicNumbers.pm
@@ -5,10 +5,9 @@ no warnings qw/numeric/;
 use Perl::Lint::Constants::Type;
 use parent "Perl::Lint::Policy";
 
-# TODO msg!
 use constant {
-    DESC => '',
-    EXPL => '',
+    DESC => 'Unnamed numeric literals make code less maintainable', # TODO
+    EXPL => 'Unnamed numeric literals make code less maintainable',
 };
 
 my $file;
@@ -44,28 +43,29 @@ sub evaluate {
     );
 
     $allow_to_the_right_of_a_fat_comma = 1;
+
+    # initializing
     if (my $this_policies_arg = $args->{prohibit_magic_numbers}) {
-        $allow_to_the_right_of_a_fat_comma = $this_policies_arg->{allow_to_the_right_of_a_fat_comma} // 1;
+        $allow_to_the_right_of_a_fat_comma =
+            $this_policies_arg->{allow_to_the_right_of_a_fat_comma} // 1;
 
         my $allowed_values = $this_policies_arg->{allowed_values};
         if (defined $allowed_values) {
-            delete $allowed_values{2};
+            delete $allowed_values{2}; # remove `2` from allowed list when allowed_values is specified
+
             for my $allowed_value (split /\s+/, $allowed_values) {
-                my ($begin, $end) = split /[.][.]/, $allowed_value;
+                my ($begin, $end) = split /[.][.]/, $allowed_value; # for range notation (e.g. `1..42`)
                 if (defined $begin && defined $end) {
-                    if (my ($delta) = $end =~ /:by\((.+)\)\z/) {
-                        for (my $num = $begin; $num <= $end; $num += $delta) {
-                            $allowed_values{$num} = 1;
-                        }
-                    }
-                    else {
-                        my $delta = 1;
-                        for (my $num = $begin; $num <= $end; $num += $delta) {
-                            $allowed_values{$num} = 1;
-                        }
+                    # used range notation
+                    my ($delta) = $end =~ /:by [(] (.+) [)] \z/x; # for range notation with by (e.g. `-2.0..2.0:by(0.5)`)
+                    $delta //= 1; # default delta
+
+                    for (my $num = $begin; $num <= $end; $num += $delta) {
+                        $allowed_values{$num} = 1;
                     }
                 }
                 else {
+                    # not used range notation
                     $allowed_values{$allowed_value} = 1;
                 }
             }
@@ -73,7 +73,8 @@ sub evaluate {
 
         my $allowed_types = $this_policies_arg->{allowed_types};
         if (defined $allowed_types) {
-            delete $allowed_types{Float};
+            delete $allowed_types{Float}; # remove `Float` from allowed types list when allowed_types is specified
+
             for my $allowed_type (split /\s+/, $allowed_types) {
                 $allowed_types{$allowed_type} = 1;
             }
@@ -110,11 +111,15 @@ sub evaluate {
 
         if ($token_type == NAMESPACE && $token_data eq 'Readonly') {
             $token = $tokens->[++$i] or last;
+
             if ($token->{type} == NAMESPACE_RESOLVER) {
                 $token = $tokens->[++$i] or last;
                 $token_data = $token->{data};
+
                 if ($token->{type} == NAMESPACE) {
                     if ($token_data eq 'Scalar' || $token_data eq 'Array') {
+                        # when `Readonly::Scalar` or `Readonly::Array`,
+                        # skip tokens to semi colon (means don't evaluate).
                         for ($i++; $token = $tokens->[$i]; $i++) {
                             $token_type = $token->{type};
                             if ($token_type == SEMI_COLON) {
@@ -124,40 +129,45 @@ sub evaluate {
                         next;
                     }
                     elsif ($token_data eq 'Array1') {
-                        $i += 2;
+                        # when `Readonly::Array1`
+                        $i += 2; # skip to assigning token
+
                         $token = $tokens->[$i];
                         $token_type = $token->{type};
                         $token_data = $token->{data};
+
                         $is_readonly_array1_ctx = 1;
-                        # no break
+
+                        # no break!
                     }
                 }
             }
         }
 
+        # for the $VERSION variable
         if (
-            ($token_type == VAR || $token_type == GLOBAL_VAR) &&
-            $token_data eq '$VERSION'
+            $token_data eq '$VERSION' &&
+            ($token_type == VAR || $token_type == GLOBAL_VAR)
         ) {
+            # skip to end of line. Don't evaluate it.
             for ($i++; $token = $tokens->[$i]; $i++) {
-                $token_type = $token->{type};
-                if ($token_type == SEMI_COLON) {
-                    last;
-                }
+                last if $token->{type} == SEMI_COLON;
             }
             next;
         }
 
         if (
             $token_type == ASSIGN ||
-            ((!$allow_to_the_right_of_a_fat_comma || $is_readonly_array1_ctx) && $token_type == ARROW)
+            ($token_type == ARROW && (!$allow_to_the_right_of_a_fat_comma || $is_readonly_array1_ctx))
         ) {
             push @violations, @{$class->_scan(\$i)};
+            next;
         }
-        elsif ($token_type == FUNCTION) {
+
+        if ($token_type == FUNCTION) {
             $token = $tokens->[++$i];
 
-            my $buf = [];
+            my $statement  = [];
             my @statements = ();
 
             my $lbnum = 1;
@@ -168,20 +178,18 @@ sub evaluate {
                     $lbnum++;
                 }
                 elsif ($token_type == RIGHT_BRACE) {
-                    if (--$lbnum <= 0) {
-                        last;
-                    }
+                    last if --$lbnum <= 0;
                 }
                 elsif ($token_type == SEMI_COLON) {
-                    push @statements, $buf;
-                    $buf = [];
+                    push @statements, $statement;
+                    $statement = [];
                 }
                 else {
-                    push @$buf, $token;
+                    push @$statement, $token;
                 }
             }
 
-            if (scalar @statements > 1) {
+            if (scalar @statements > 1) { # when exists multiple statements in function
                 my $last_statement = pop @statements;
 
                 my $return_value_token = pop @$last_statement or next;
@@ -189,38 +197,40 @@ sub evaluate {
                     $return_value_token = pop @$last_statement or next;
                 }
 
-                if (
-                    $return_value_token->{type} == INT ||
-                    $return_value_token->{type} == DOUBLE
-                ) {
-                    # TODO it should support type constraint
-                    if (!$allowed_values{$return_value_token->{data}+0}) { # `+0` to convert to number
-                        push @violations, {
-                            filename => $file,
-                            line     => $token->{line},
-                            description => DESC,
-                            explanation => EXPL,
-                            policy => __PACKAGE__,
-                        };
-                    }
+                my $invalid_token;
+                if ($return_value_token->{type} == INT) {
+                    $invalid_token = $class->_validate_int_token($return_value_token);
+                }
+                elsif ($return_value_token->{type} == DOUBLE) {
+                    $invalid_token = $class->_validate_doble_token($return_value_token);
+                }
+
+                if ($invalid_token) {
+                    push @violations, {
+                        filename => $file,
+                        line     => $token->{line},
+                        description => DESC,
+                        explanation => EXPL,
+                        policy => __PACKAGE__,
+                    };
                 }
             }
+
+            next;
         }
-        elsif ($token_type == FOR_STATEMENT || $token_type == FOREACH_STATEMENT) {
+
+        if ($token_type == FOR_STATEMENT || $token_type == FOREACH_STATEMENT) {
             for ($i++; $token = $tokens->[$i]; $i++) {
                 $token_type = $token->{type};
 
-                if ($token_type == SEMI_COLON || $token_type == LEFT_BRACE) { # XXX
+                if ($token_type == SEMI_COLON || $token_type == LEFT_BRACE) {
                     last;
                 }
-                elsif ($token_type == SLICE) {
+                elsif ($token_type == SLICE) { # e.g. for my $foo (1..42)
                     my $begin = $tokens->[$i-1] or last;
                     my $end   = $tokens->[$i+1] or last;
                     if ($begin->{type} == INT && $end->{type} == INT) {
-                        if (
-                            !$allowed_values{$begin->{data}} ||
-                            !$allowed_values{$end->{data}}
-                        ) {
+                        if (!$allowed_values{$begin->{data}} || !$allowed_values{$end->{data}}) {
                             push @violations, {
                                 filename => $file,
                                 line     => $token->{line},
@@ -232,8 +242,11 @@ sub evaluate {
                     }
                 }
             }
+            next;
         }
-        elsif ($token_type == LEFT_BRACKET) {
+
+        # for index of array
+        if ($token_type == LEFT_BRACKET) {
             my $next_token = $tokens->[$i+1] or last;
             if ($next_token->{type} == INT) {
                 my $int_token = $next_token;
@@ -260,6 +273,7 @@ sub evaluate {
                     };
                 }
             }
+            next;
         }
     }
 
@@ -296,48 +310,10 @@ sub _scan {
 
     my @violations;
     if ($token_type == DOUBLE) {
-        if ($allowed_types{Float} && $allowed_values{$token_data+0}) { # `+0` to convert to number
-            my $next_token = $tokens->[$$i+1];
-            if ($next_token && $next_token->{type} == DOUBLE) {
-                $invalid_token = $next_token;
-            }
-        }
-        elsif (!$allowed_values{all_integers} || $token_data !~ /[.]0+\z/) {
-            $invalid_token = $token;
-        }
+        $invalid_token = $class->_validate_doble_token($token, $$i);
     }
     elsif ($token_type == INT) {
-        if (my ($base_type) = $token_data =~ /\A[0-9]([b0xe]).+\z/) {
-            if ($1 eq 'b') {
-                if (!$allowed_types{Binary}) {
-                    $invalid_token = $token;
-                }
-            }
-            elsif ($1 eq '0') {
-                if (!$allowed_types{Octal}) {
-                    $invalid_token = $token;
-                }
-            }
-            elsif ($1 eq 'x') {
-                if (!$allowed_types{Hex}) {
-                    $invalid_token = $token;
-                }
-            }
-            elsif ($1 eq 'e') {
-                if (!$allowed_types{Exp}) {
-                    $invalid_token = $token;
-                }
-            }
-        }
-
-        if (!$invalid_token) {
-            if (!$allowed_types{Int}) {
-                $invalid_token = $token;
-            }
-            elsif (!$allowed_values{all_integers} && !$allowed_values{$token_data+0}) { # `+0` to convert to number
-                $invalid_token = $token;
-            }
-        }
+        $invalid_token = $class->_validate_int_token($token);
     }
     elsif ($token_type == LEFT_PAREN) {
         my $lpnum = 1;
@@ -382,6 +358,55 @@ sub _scan {
     }
 
     return \@violations;
+}
+
+sub _validate_int_token {
+    my ($class, $token) = @_;
+
+    my $token_data = $token->{data};
+
+    if (my ($base_type) = $token_data =~ /\A[0-9]([b0xe]).+\z/) {
+        if ($1 eq 'b') {
+            return $token if !$allowed_types{Binary};
+        }
+        elsif ($1 eq '0') {
+            return $token if !$allowed_types{Octal};
+        }
+        elsif ($1 eq 'x') {
+            return $token if !$allowed_types{Hex};
+        }
+        elsif ($1 eq 'e') {
+            return $token if !$allowed_types{Exp};
+        }
+    }
+
+    if (!$allowed_types{Int}) {
+        return $token;
+    }
+
+    if (!$allowed_values{all_integers} && !$allowed_values{$token_data+0}) { # `+0` to convert to number
+        return $token;
+    }
+
+    return;
+}
+
+sub _validate_doble_token {
+    my ($class, $token, $i) = @_;
+
+    my $token_data = $token->{data};
+
+    if ($i && $allowed_types{Float} && $allowed_values{$token_data+0}) { # `+0` to convert to number
+        my $next_token = $tokens->[$i+1];
+        if ($next_token && $next_token->{type} == DOUBLE) {
+            return $next_token;
+        }
+    }
+    elsif (!$allowed_values{all_integers} || $token_data !~ /[.]0+\z/) {
+        return $token;
+    }
+
+    return;
 }
 
 1;
